a67e7edf7a39b751303b22b5a8b778e959a8e159
orielvf <orielvf@gmail.com>
2024-06-19 15:41:20 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import HelloWorld from './components/HelloWorld'
+
+
+function App() {
+
+  return (
+    <>
+    <HelloWorld/>      
+    </>
+  )
+}
+export default App
