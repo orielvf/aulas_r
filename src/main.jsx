0123446bff679cc5fce954b0fc204efe8b675ace
orielvf <orielvf@gmail.com>
2024-06-23 18:48:49 -0300

    React-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Configurando o react-router
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+
+import Home from './assets/routes/Home.jsx'
+import Contact from './assets/routes/Contact.jsx'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "contact",
+    element: <Contact/>,
+  },
+]);
+// Configurando o react-router
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
